9fad5e48a78ecdd1621909b5b6a54b120d7e4da1
Eldar Oralov <eldar.oralov@gmail.com>
2020-01-05 11:03:45 +0600

    Project files added

---
+++ HW9/HW9/DataAccess/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using HW9.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW9.DataAccess
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Item> Items { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Item>()
+                .HasData(
+                new Item
+                {
+                    Name = "Item1",
+                    Description = "This is item number one"
+                },
+                new Item
+                {
+                    Name = "Item2",
+                    Description = "This is item number two"
+                });
+        }
+    }
+}
---
+++ HW9/HW9/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW9.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
